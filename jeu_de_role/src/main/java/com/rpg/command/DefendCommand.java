e6bb5c521052eeb3368ea1ae3555ce8a6b05d6c0
Sab Ben <sbenseghir.sb@gmail.com>
2025-10-01 11:47:09 +0200

    Implémentation des actions du jeu (attaque, défense, utilisation de pouvoir)

---
+++ jeu_de_role/src/main/java/com/rpg/command/DefendCommand.java
@@ -0,0 +1,19 @@
+package com.rpg.command;
+
+import com.rpg.battle.Battle;
+import com.rpg.battle.BattleUnit;
+
+/** Commande "défendre". */
+public class DefendCommand implements GameCommand {
+    private final Battle battle;
+    private final BattleUnit who;
+
+    public DefendCommand(Battle battle, BattleUnit who) {
+        this.battle = battle; this.who = who;
+    }
+
+    @Override public void execute() { battle.defend(who); }
+    @Override public void undo() { /* no-op dans cette version */ }
+    @Override public String label() { return who.getCharacter().getName() + " se défend"; }
+}
+
